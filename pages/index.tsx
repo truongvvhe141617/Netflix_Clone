39c05bdf96a99d88e6440543c7676a915e9c5e1c
truong.vuong <truong.vuong@ekoios.vn>
2023-04-25 11:56:20 +0700

    update menu navvar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next"
 import { getSession, signOut } from "next-auth/react"
-
+import NavBar from '@/components/NavBar'
 export async function getServerSideProps(context: NextPageContext){
         const session = await getSession(context);
        if(!session){
@@ -20,12 +20,13 @@ export async function getServerSideProps(context: NextPageContext){
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  // const { data: user } = useCurrentUser();
   return (
     <>
-    <h1 className="text-2xl text-green-500">Netlfix Clone</h1>
-    <p className="text-white">Logged in as: {user?.name}</p>
-    <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout</button>
+    {/* <h1 className="text-xl text-red-600">Netlfix</h1>
+    <p className="text-gray-500">Logged in as: {user?.name}</p>
+    <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout</button> */}
+    <NavBar/>
     </>
   )
 }
