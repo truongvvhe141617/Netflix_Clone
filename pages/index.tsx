c0fc1f9c0ea18e6340fd439551a47ccb317a87a7
truong.vuong <truong.vuong@ekoios.vn>
2023-04-26 22:08:44 +0700

    update movie & billboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next"
 import { getSession, signOut } from "next-auth/react"
 import NavBar from '@/components/NavBar'
+import Billboard from "@/components/Billboard";
 export async function getServerSideProps(context: NextPageContext){
         const session = await getSession(context);
        if(!session){
@@ -20,13 +20,10 @@ export async function getServerSideProps(context: NextPageContext){
 }
 
 export default function Home() {
-  // const { data: user } = useCurrentUser();
   return (
     <>
-    {/* <h1 className="text-xl text-red-600">Netlfix</h1>
-    <p className="text-gray-500">Logged in as: {user?.name}</p>
-    <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout</button> */}
     <NavBar/>
+    <Billboard/>
     </>
   )
 }
